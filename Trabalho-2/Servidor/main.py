7043e529df61b1e3a43f20d2e036edf15d55f4e9
Willians-S-S <09willians@gmail.com>
2025-03-19 09:45:28 -0300

    Feat: Add tocar e parar alarme

--- Trabalho-2/Servidor/main.py
+++ Trabalho-2/Servidor/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from image import save_image
+import pygame
 
 app = FastAPI()
 
@@ -7,3 +8,16 @@ app = FastAPI()
 def upload_image(image: UploadFile = File(...)):
     image_db = save_image(image)
     print(f"Imagem salva com sucesso: {image_db}")
+    pygame.mixer.init()
+
+    pygame.mixer.music.load('/home/will/Documentos/UFPI/SD/Trabalho-2/Servidor/alarme/Sirene de 6 tons demonstração.mp3')
+
+    pygame.mixer.music.play()
+
+    while True:
+        sair = input("Escreva sair para sair: ")
+        if sair == "sair":
+            pygame.mixer.music.stop()
+            break
+    
+    return {"Mensagem": "Alarme desativado"}
